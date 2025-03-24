a1dbf2296bdf6ea376675585c8ed52ed37a85607
mytkom <marek.mytkowski.mm@gmail.com>
2025-03-24 23:39:13 +0100

    method improvements and regularization path plot (still probably not correct)

--- src/log_reg_ccd.py
+++ src/log_reg_ccd.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from numpy.typing import NDArray
 from sklearn.model_selection import KFold
 from .measures import ProbMeasure, ClassMeasure
@@ -20,7 +21,7 @@ class LogRegCCD:
         self,
         X_train: NDArray[np.float64],
         y_train: NDArray[np.int_],
-        eps=1e-7,
+        eps=1e-3,
         lam_max=10,
         lam_count=100,
         k_fold=10,
@@ -59,9 +60,6 @@ class LogRegCCD:
             lambda_mse = {lam: [] for lam in self.lambdas}
             lambda_betas = { lam: [] for lam in self.lambdas }
 
-            # Initialize with zeros
-            current_beta = np.zeros(in_features + 1)
-
             # k-fold cross-validation
             kf = KFold(n_splits=k_fold, shuffle=True)
             for fold, (train_idx, valid_idx) in enumerate(kf.split(X_train)):
@@ -73,15 +71,17 @@ class LogRegCCD:
                 self._log(f"Train shape: {cw_X_train.shape}")
                 self._log(f"Validation shape: {cw_X_valid.shape}")
 
+                # Initialize with zeros
+                current_beta = np.zeros(in_features + 1)
+
+
                 for lam in self.lambdas:
                     new_beta = self._coordinate_descent(
                         cw_X_train, cw_y_train, lam, current_beta
                     )
 
                     # Validation step
-                    val_predictions = 1 / (
-                        1 + np.exp(-(cw_X_valid @ new_beta[1:]) + new_beta[0])
-                    )
+                    val_predictions = np.clip(self._sigmoid(-(cw_X_valid @ new_beta[1:]) + new_beta[0]), 1e-10, 1 - 1e-10)
                     # Mean deviance
                     val_loss = -2 * np.mean(cw_y_valid * np.log(val_predictions) + (1 - cw_y_valid) * np.log(1 - val_predictions))
 
@@ -127,30 +127,23 @@ class LogRegCCD:
 
     def predict_proba(self, X, beta_idx=None):
         beta = self.best_beta if not beta_idx else self.betas[beta_idx]
-        return 1 / (1 + np.exp(-(X @ beta[1:]) + beta[0]))
+        return self._sigmoid((-X @ beta[1:]) + beta[0])
 
     def predict(self, X, beta_idx=None):
         beta = self.best_beta if not beta_idx else self.betas[beta_idx]
         return ((beta[0] + (X @ beta[1:])) > 0.0).astype("int")
 
     def _coordinate_descent(
-        self, X, y, lam: float, beta: NDArray[np.float64], max_iter=2, eps=1e-10
+        self, X, y, lam: float, beta: NDArray[np.float64], max_iter=1000, eps=1e-10
     ) -> NDArray[np.float64]:
         n = X.shape[0]
         X = np.hstack((np.ones((X.shape[0], 1)), X))
+        l = float("inf")
 
-        for _ in range(1, max_iter):
-            # TODO: stop condition
-
-            # Clip to avoid overflow
-            posteriors = 1.0 / (1.0 + np.exp(-np.clip(X @ beta, -500, 500)))
+        for i in range(0, max_iter):
+            atol = 1e-3
+            posteriors = np.clip(self._sigmoid(-(X @ beta)), atol, 1.-atol)
             weights = posteriors * (1 - posteriors)
-
-            # Make values close to 0 and 1 exactly 0 and 1 like at page 9 of src paper
-            atol = 1e-5
-            is_close = np.isclose(posteriors, 0, atol=atol) | np.isclose(posteriors, 1, atol=atol)
-            posteriors[is_close] = np.round(posteriors[is_close])
-            weights[is_close] = atol
 
             # Calculate common part of partial derivatives over beta_j
             y_minus_posteriors = y - posteriors
@@ -158,21 +151,77 @@ class LogRegCCD:
 
             # Calculate partial derivatives of beta_j for every j
             for j in range(0, beta.shape[0]):
-                denom = (weights * X[:, j]).T @ X[:, j]
-                result = ((X[:, j].T @ y_minus_posteriors) + (beta[j] * denom)) / n
+                denom = (np.square(X[:, j]).T @ weights)
+                result = ((X[:, j].T @ y_minus_posteriors) + (beta[j] * denom))
                 if j == 0:
                     # Intercept shouldn't be regularized
                     new_beta[j] = result / denom
                 else:
                     # Penalty included using soft threshold function
-                    new_beta[j] = self._soft_thresh(result, np.float64(lam)) / denom
+                    new_beta[j] = self._soft_thresh(result, n * np.float64(lam)) / denom
+            if not np.isclose(np.linalg.norm(new_beta), 0.):
+                new_beta = new_beta / np.linalg.norm(new_beta)
+
+            l_old = l
+            # Exact loss
+            # l = (-np.sum((y @ (X @ new_beta)) - np.log(1 + np.exp(np.clip(X @ new_beta, -500, 500)))) / n) + lam * np.linalg.norm(new_beta, ord=1)
+            # Quadratic approx
+            l = -1/(2*n) * weights.T @ np.square(X @ beta + (y_minus_posteriors / weights) - X @ new_beta)        
+
+            # Stop condition
+            if abs(l - l_old) < eps:
+                self._log(f"Early stopping Coordinate Descent in iteration: {i}")
+                break
 
             beta = new_beta
 
         return beta
 
+    # def _sigmoid(self, s, clip=300):
+    #     return 1.0 / (1.0 + np.exp(np.clip(s, -clip, clip)))
+
+    # https://stackoverflow.com/questions/51976461/optimal-way-of-defining-a-numerically-stable-sigmoid-function-for-a-list-in-pyth
+    def _positive_sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+    def _negative_sigmoid(self, x):
+        # Cache exp so you won't have to calculate it twice
+        exp = np.exp(x)
+        return exp / (exp + 1)
+
+
+    def _sigmoid(self, x):
+        positive = x >= 0
+        # Boolean array inversion is faster than another comparison
+        negative = ~positive
+
+        # empty contains junk hence will be faster to allocate
+        # Zeros has to zero-out the array after allocation, no need for that
+        # See comment to the answer when it comes to dtype
+        result = np.empty_like(x, dtype=np.float64)
+        result[positive] = self._positive_sigmoid(x[positive])
+        result[negative] = self._negative_sigmoid(x[negative])
+
+        return result
+  
     def _soft_thresh(self, z, gamma):
         return np.sign(z) * np.maximum(np.abs(z) - gamma, 0)
+
+    def plot_lasso_path(self):
+        """
+        Plots the Lasso path with -log(lambda) on the x-axis and beta values on the y-axis.
+        """
+        plt.figure(figsize=(8, 6))
+        for i in range(1, self.betas.shape[1]):
+            plt.plot(self.lambdas, self.betas[:, i], label=f'{i+1}')
+        
+        plt.xscale("log")
+        plt.xlabel('lambda')
+        plt.ylabel('Beta coefficients')
+        plt.title('Lasso Regularization Path')
+        plt.grid(True)
+        plt.show()
 
     def _log(self, message) -> None:
         """
