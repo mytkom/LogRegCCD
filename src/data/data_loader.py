90206ab8917dfb17c50cb4ca2d22aa8487039da5
werag55 <147431062+werag55@users.noreply.github.com>
2025-03-31 19:46:18 +0200

    Synthetic data experiments (#7)
    * Synthetic data experiments
    
    * Forgot to save notebook
    
    * Pylint + fix
    
    * Updated synthetic data experiments
    
    * Typo
    
    * Run analyses
    
    * Forgot to save
    
    * adjust algorithm correctness test to new synthetic dataset generating
    
    * Typo
    
    ---------
    
    Co-authored-by: mytkom <marek.mytkowski.mm@gmail.com>

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -32,38 +32,34 @@ class OpenMLDataLoader(DataLoader):
 class SyntheticDataLoader(DataLoader):
     """Synthetic data creator."""
 
-    def __init__(self, p, n, d, g):
+    def __init__(self, p, n, d, g, random_seed=42): # pylint: disable=too-many-arguments, too-many-positional-arguments
         """
         Parameters:
         - p: Class prior probability for Y=1.
         - n: Number of observations.
         - d: Number of features.
         - g: Covariance parameter for the multivariate normal distribution.
+        - random_seed: Random seed.
         """
         self.p = p
         self.n = n
         self.d = d
         self.g = g
+        self.random_seed = random_seed
 
-    # pylint: disable=fixme
-    # TODO check - nie jestem pewna czy to jest dobrze, dodałam jako przykład factory
     def load_data(self):
         """Generate synthetic data."""
 
-        labels = np.random.binomial(1, self.p, self.n)
+        np.random.seed(self.random_seed)
+        labels = np.random.binomial(1, self.p, size=self.n)
+        indices = np.arange(self.d)
+        cov_matrix = self.g ** np.abs(indices[:, None] - indices)
+        mean_0 = np.zeros(self.d)
+        mean_1 = np.array([1 / (k + 1) for k in range(self.d)])
 
-        cov_matrix = np.zeros((self.d, self.d))
-        for i in range(self.d):
-            for j in range(self.d):
-                cov_matrix[i, j] = self.g ** abs(i - j)
+        data = np.random.multivariate_normal(mean_0, cov_matrix, size=self.n)
+        data[labels == 1] = np.random.multivariate_normal(mean_1, cov_matrix, size=np.sum(labels))
+        feature_names = [f'feature_{i}' for i in range(self.d)]
+        data = pd.DataFrame(data, columns=feature_names)
 
-        data = np.zeros((self.n, self.d))
-        for i in range(self.n):
-            if labels[i] == 0:
-                mean = np.zeros(self.d)
-            else:
-                mean = np.array([1 / (k + 1) for k in range(self.d)])
-            data[i, :] = np.random.multivariate_normal(mean=mean, cov=cov_matrix)
-
-        data = pd.DataFrame(data, columns=[f'feature_{i}' for i in range(self.d)])
         return data, labels
